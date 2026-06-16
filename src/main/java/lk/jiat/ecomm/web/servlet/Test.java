b31c9b6b7a8856ca64abec78db1000da1648e07c
kalhara05 <kalhara139@gmail.com>
2026-06-16 14:06:16 +0530

    Add EJB client dep and JNDI lookup in Test
    Add lk.jiat:ecomm-user (ejb-client) dependency to pom.xml and update Test servlet to perform a JNDI lookup for the UserRemote session bean. The servlet uses InitialContext.lookup("java:global/ecomm_user_ejb_exploded/UserSessionBean") to obtain UserRemote, calls getAllUsers(), and iterates the returned UserDTO list. Added necessary imports and basic exception handling.

--- src/main/java/lk/jiat/ecomm/web/servlet/Test.java
+++ src/main/java/lk/jiat/ecomm/web/servlet/Test.java
@@ -5,8 +5,12 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lk.jiat.ecomm.user.dto.UserDTO;
+import lk.jiat.ecomm.user.remote.UserRemote;
 
+import javax.naming.InitialContext;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/test")
 public class Test extends HttpServlet {
@@ -14,5 +18,22 @@ public class Test extends HttpServlet {
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
        resp.getWriter().write("Ecomm Web Module Test");
+
+       try {
+
+           InitialContext ic = new InitialContext();
+           UserRemote userRemote = (UserRemote)
+                   ic.lookup("java:global/ecomm_user_ejb_exploded/UserSessionBean");
+
+          List<UserDTO> allUsers = userRemote.getAllUsers();
+          for (UserDTO user : allUsers) {
+              user.toString();
+          }
+
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+
     }
+
 }
